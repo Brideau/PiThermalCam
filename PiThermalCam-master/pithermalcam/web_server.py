cd9dafe4cf4f7986678d97ac30569ee31a283976
Ryan Brideau <ryan.brideau@gmail.com>
2022-04-10 23:21:47 -0400

    Modified to support button push

--- PiThermalCam-master/pithermalcam/web_server.py
+++ PiThermalCam-master/pithermalcam/web_server.py
@@ -4,23 +4,22 @@
 # Flask web server for MLX90640 Thermal Camera w Raspberry Pi
 # If running directly, run from root folder, not pithermalcam folder
 ##################################
-from pithermalcam import pithermalcam
-
+try:  # If called as an imported module
+	from pithermalcam import pithermalcam
+except:  # If run directly
+	from pi_therm_cam import pithermalcam
 from flask import Response, request
 from flask import Flask
 from flask import render_template
 import threading
 import time, socket, logging, traceback
 import cv2
-import RPi.GPIO as GPIO
 
 # Set up Logger
 logging.basicConfig(filename='pithermcam.log',filemode='a',
 					format='%(asctime)s %(levelname)-8s [%(filename)s:%(name)s:%(lineno)d] %(message)s',
 					level=logging.WARNING,datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
-
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # initialize the output frame and a lock used to ensure thread-safe exchanges of the output frames (useful when multiple browsers/tabs are viewing the stream)
 outputFrame = None
@@ -40,9 +39,6 @@ def index():
 def save_image():
 	thermcam.save_image()
 	return ("Snapshot Saved")
-
-def save_image_button(channel):
-	save_image()
 
 @app.route('/units')
 def change_units():
@@ -132,14 +128,11 @@ def generate():
 		# yield the output frame in the byte format
 		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
 
-def start_server(output_folder = '/home/brideau/'):
+def start_server(output_folder:str = '/home/pi/pithermalcam/saved_snapshots/'):
 	global thermcam
 	# initialize the video stream and allow the camera sensor to warmup
-	thermcam = pithermalcam(output_folder=output_folder, use_f=False)
+	thermcam = pithermalcam(output_folder=output_folder)
 	time.sleep(0.1)
-
-	GPIO.add_event_detect(25, GPIO.RISING, callback=save_image_button)
-	
 
 	# start a thread that will perform motion detection
 	t = threading.Thread(target=pull_images)
@@ -152,11 +145,9 @@ def start_server(output_folder = '/home/brideau/'):
 	print(f'Server can be found at {ip}:{port}')
 
 	# start the flask app
-	app.run(host=ip, port=port, debug=True, threaded=True, use_reloader=False)
+	app.run(host=ip, port=port, debug=False,threaded=True, use_reloader=False)
 
 
 # If this is the main thread, simply start the server
 if __name__ == '__main__':
-	start_server(
-		output_folder='/home/brideau/'
-	)
+	start_server()
